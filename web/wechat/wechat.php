ac46ea75776ee61df5466f25999498edc94eb251
liyake04@gmail.com <liyake04@gmail.com@112a1d2d-8a66-39b0-3757-4046d3d5969e>
2013-09-18 14:43:10 +0000

    1、重新对菜单进行了分类。 2、对『天主教小助手』的各菜单设置了对应的回复。

--- web/wechat/wechat.php
+++ web/wechat/wechat.php
@@ -37,7 +37,7 @@ class wechatCallbackapiTest
 			$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
 			if($postObj->Event=="CLICK")
 			{
-				$resultStr = $this->getDefaltReply($postObj);
+				$resultStr = $this->getEventReply($postObj);
 			}
 			else
 			{
@@ -82,12 +82,13 @@ class wechatCallbackapiTest
 		}
 	}
 	
-	private function getDefaltReply($postObj)
+	private function getDefaltReply($postObj,$desc='')
 	{
 		$fromUsername = $postObj->FromUserName;
 		$toUsername = $postObj->ToUserName;
 		$time = time();
-		$desc = "感谢你的关注，我们正在努力完善『天主教小助手』，后续会为你带来更多的好功能！耶稣爱你！ ^_^";
+		if($desc=='')
+			$desc = "感谢你的关注，我们正在努力完善『天主教小助手』，后续会为你带来更多的好功能！耶稣爱你！ ^_^";
 		$textTpl = '<xml>
 			<ToUserName><![CDATA[%s]]></ToUserName>
 			<FromUserName><![CDATA[%s]]></FromUserName>
@@ -101,18 +102,78 @@ class wechatCallbackapiTest
 	
 	private function getEventReply($postObj)
 	{
-		$modemap = array (
-		'1' => 'mass',
-		'2' => 'med',
-		'3' => 'lod',
-		'4' => 'ves',
-		'5' => 'comp',
-		'6' => 'let',
-		'7' => 'thought',
-		'8' => 'ordo',
-		'9' => 'saint',
-		);
 		$key = $postObj->EventKey;
+		
+		$ArtCount = 0;
+		$Articles = "";
+		$resultStr='';
+		
+		if($key=='102')
+		{
+			//美文推荐
+			return $this->getDefaltReply($postObj,'『美文推荐』模块正在努力设计中...感谢你的关注！');
+		}
+		else if($key=='201')
+		{
+			//日课及读经
+			$modemap = array (
+			'1' => 'mass',
+			'2' => 'med',
+			'3' => 'lod',
+			'4' => 'ves',
+			'5' => 'comp',
+			'6' => 'let',
+			'7' => 'thought',
+			'8' => 'ordo',
+			'9' => 'saint',
+			);
+			
+			$url = ROOT_WEB_URL."getstuff/getstuff.php?date=".gmdate("Y-m-d",time()+3600*8);
+			$json = json_decode(file_get_contents($url),true);
+			foreach ($modemap as $key => $value)
+			{
+				if(isset($json[$value]))
+				{
+					$Articles = $Articles.$this->getSubArticle($value,trim(convert_html_to_text($json[$value])),0,0);
+					$ArtCount = $ArtCount+1;
+				}
+			}
+		}
+		else if($key=='303')
+		{
+			//圣经金句
+			$onelodo = "";
+			$result = mysql_query("select lodo from lodo L JOIN (SELECT CEIL(MAX(ID)*RAND()) AS ID FROM lodo) AS m ON L.ID >= m.ID LIMIT 1;");
+			if ($row = mysql_fetch_array($result))
+			{
+				$onelodo = $row['lodo'];
+			}
+			return $this->getDefaltReply($postObj,$onelodo);
+		}
+		else if($key=='304')
+		{
+			//各地教堂
+			return $this->getDefaltReply($postObj,'『各地教堂』模块正在努力设计中...后续会在该模块中展示各地的教堂及弥撒时间，感谢你的关注！');
+		}
+		
+		if($Articles!="")
+		{
+			$textTpl = '<xml>
+				<ToUserName><![CDATA[%s]]></ToUserName>
+				<FromUserName><![CDATA[%s]]></FromUserName>
+				<CreateTime>%s</CreateTime>
+				<MsgType><![CDATA[news]]></MsgType>
+				<ArticleCount>%s</ArticleCount>
+				<Articles>%s
+				</Articles>
+				<FuncFlag>1</FuncFlag>
+				</xml>';
+			return sprintf($textTpl, $postObj->FromUserName, $postObj->ToUserName, time(), $ArtCount, $Articles);
+		}
+		else
+		{
+			return $this->getDefaltReply($postObj);
+		}
 	}
 	
 	private function getTextReply($postObj)
