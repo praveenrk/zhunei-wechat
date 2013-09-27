7dee8fa8bc0bb5bdb7836d31411d6f329fc42761
liyake04@gmail.com <liyake04@gmail.com@112a1d2d-8a66-39b0-3757-4046d3d5969e>
2013-09-27 08:18:29 +0000

    增加歌曲管理的页面

---
+++ trunk/web/music/singer.php
@@ -0,0 +1,53 @@
+<?php
+	require_once("../include/dbconn.php");
+	require_once("../include/define.php");
+	require_once("../users/user.class.php");
+	
+	$id = -1;
+	if(isset($_GET['id']))
+	{
+		$id = (int)$_GET['id'];
+	}
+	
+	$web_title = "";
+	$name = "";
+	$result = mysql_query('select * from singer where id='.$id.';');
+	if($row = mysql_fetch_array($result))
+	{
+		$name = $row['name'];
+		$web_title = $name.'——天主教小助手';
+	}
+	else
+	{
+		header("Location: index.php");
+		exit();
+	}
+	echo('<html><head><title>'.$web_title.'</title><meta http-equiv=Content-Type content="text/html;charset=utf-8"><meta name="viewport" content="user-scalable=no, width=device-width" /><link rel="stylesheet" type="text/css" href="singer.css"/>');
+	if(User::isAdmin())
+	{echo('<script type="text/javascript">function delSinger(id)
+		{
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange=function()
+		{
+		if (xmlhttp.readyState==4 && xmlhttp.status==200)
+		{
+			window.location.href="index.php";
+			return;
+		}
+		}
+		xmlhttp.open("POST","./music_op.php",true);
+		xmlhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+		xmlhttp.send("mode=delsinger&id="+id);
+		}</script>');
+	}
+	echo('</head><body><center>');
+	echo('<div class="singer"><h3>'.$name.'</h3></div><br/>');
+	
+	$result = mysql_query('select * from alume where singer='.$id.';');
+	while($row = mysql_fetch_array($result))
+	{
+		echo('<div class="alume"><a href="alume.php?id='.$row['id'].'">'.$row['name'].'</a><img src="'.$row['pic'].'"/></div>');
+	}
+	if(User::isAdmin()){echo('<br/><br/><br/><a style="display:block;clear:left;" href="#" onclick="delSinger('.$id.')">删除</a>');}
+	echo('</center></body></html>');
+ ?>
