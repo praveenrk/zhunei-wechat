2e4e8589f8643349bc1d2086bbeb44c050f849da
liyake04@gmail.com <liyake04@gmail.com@112a1d2d-8a66-39b0-3757-4046d3d5969e>
2013-10-08 07:09:01 +0000

    1、修复微信分享功能script未闭合的bug 2、增加梵蒂冈广播分享时的图标

--- trunk/web/include/define.php
+++ trunk/web/include/define.php
@@ -3,7 +3,7 @@ define("ROOT_WEB_URL", "http://t.liyake.com/");
 
 function getWechatShareScript($link,$title,$imgurl)
 {
-	return '</script><script language="javascript" type="text/javascript">var contentModel={
+	return '<script language="javascript" type="text/javascript">var contentModel={
 	"img_url": "'.$imgurl.'", "img_width": "","img_height": "","link": "'.$link.'", "desc": "'.$title.'", 
     "title": "'.$title.'", "src": "天主教小助手" };</script><script language="JavaScript" src="http://mat1.gtimg.com/www/js/newsapp/wechat/wechat20130809_min.js" type="text/javascript" charset="utf-8"></script>';
 }
