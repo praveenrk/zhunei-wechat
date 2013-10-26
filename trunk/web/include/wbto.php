d2e72ef067d4c2c2e9929b2bce0fac56ce73e5ce
liyake04@gmail.com <liyake04@gmail.com@112a1d2d-8a66-39b0-3757-4046d3d5969e>
2013-10-26 13:29:22 +0000

    修改微博发送的实现逻辑

--- trunk/web/include/wbto.php
+++ trunk/web/include/wbto.php
@@ -1,9 +1,11 @@
 <?php
+require_once("../include/dbconn.php");
+
 //用于发送微博到微博通
-function send_to_wbto($content)
+function send2wbto($content)
 {
 	$username = '天主教小助手';
-	$password = 'password';
+	$password = 'pass';
 	
 	$fields = array();
 	$fields['source'] = 'wordpress';
@@ -21,6 +23,21 @@ function send_to_wbto($content)
 	curl_close($ch);  
 }
 
+function add2weibolist($content)
+{
+	mysql_query('insert into weibolist(text) values ("'.mysql_real_escape_string($content).'");');
+}
+
+function check2updateweibo()
+{
+	$result = mysql_query('select id,text from weibolist order by id desc limit 1;');
+	while ($row = mysql_fetch_array($result))
+	{
+		send2wbto($row['text']);
+		echo('send to wbto "'.$row['text'].'"');
+		mysql_query('delete from weibolist where id='.$row['id'].';');
+	}
+}
 //how to use
 //send_to_wbto('欢迎大家关注天主教小助手的微博');
 ?>
