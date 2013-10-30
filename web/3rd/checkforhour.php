33d76fd7ed4954002ec4bfd891942baa952c7df5
liyake04@gmail.com <liyake04@gmail.com@112a1d2d-8a66-39b0-3757-4046d3d5969e>
2013-10-30 01:39:55 +0000

    修改梵蒂冈广播的实现方式（采用动态抓取rss）

--- web/3rd/checkforhour.php
+++ web/3rd/checkforhour.php
@@ -3,6 +3,9 @@
 	echo('<h1>获取梵蒂冈电台文章</h1>');
 	echo(file_get_contents('http://t.liyake.com/vaticanacn/checkrss.php'));
 	
+	echo('<h1>获取梵蒂冈电台广播</h1>');
+	echo(file_get_contents('http://t.liyake.com/media/checkrss.php'));
+	
 	echo('<h1>获取信德网文章</h1>');
 	echo(file_get_contents('http://t.liyake.com/faithlife/caiji.php'));
 	
