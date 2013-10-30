964971be8262a3a803e807c3c79576edce0a0674
liyake04@gmail.com <liyake04@gmail.com@112a1d2d-8a66-39b0-3757-4046d3d5969e>
2013-10-30 01:39:55 +0000

    修改梵蒂冈广播的实现方式（采用动态抓取rss）

---
+++ trunk/web/media/checkrss.php
@@ -0,0 +1,46 @@
+<?php
+	require_once("../include/define.php");
+	function getAudio($rss,&$link)
+	{
+		$rsscontent = file_get_contents($rss);
+		$rss = simplexml_load_string($rsscontent);
+		$channel = $rss->channel;
+		if(count($channel->item)>0)
+		{
+			$item = $channel->item[0];
+			$enclosure = $item->enclosure;
+			$link = $enclosure['url'];
+			return date("Y-m-d", strtotime($item->pubDate));
+		}
+	}
+	
+	$cnday = "http://media.vaticanradiowebcast.org/mp3_od/cinese_1.mp3";
+	$cndaytitle = "中文广播";
+	$enmorning = "http://media.vaticanradiowebcast.org/mp3_od/rg_inglese_1_1.mp3";
+	$enmtitle = "English News(morning)";
+	$enevening = "http://media.vaticanradiowebcast.org/mp3_od/rg_inglese_2_1.mp3";
+	$enetitle = "English News(evening)";
+	$cnmass = "http://media.vaticanradiowebcast.org/mp3_od/messa_cinese_1.mp3";
+	$cnmasstitle = "主日弥撒";
+	
+	{
+		$cndaytitle = '中文广播('.getAudio("http://media01.vatiradio.va/podmaker/podcaster.aspx?c=cinese",$cnday).')';
+		$enmtitle = 'English News('.getAudio("http://media01.vatiradio.va/podmaker/podcaster.aspx?c=rg_inglese_1",$enmorning).' morning)';
+		$enetitle = 'English News('.getAudio("http://media01.vatiradio.va/podmaker/podcaster.aspx?c=rg_inglese_2",$enevening).' evening)';
+		$cnmasstitle = '主日弥撒('.getAudio("http://media01.vatiradio.va/podmaker/podcaster.aspx?c=messa_cinese",$cnmass).')';
+	}
+	
+	$fp = fopen("vaticanradio.html","w");
+	fwrite($fp,'<html><head><title>梵蒂冈广播电台——天主教小助手</title><meta http-equiv=Content-Type content="text/html;charset=utf-8"><meta name="viewport" content="user-scalable=no, width=device-width" /><link rel="stylesheet" type="text/css" href="vaticanradiowebcast_cn.css"/></head><body><center><h2>梵蒂冈广播电台</h2><h3>'.$cndaytitle.'</h3>
+	<audio src="'.$cnday.'" controls></audio><h3>'.$enmtitle.'</h3>
+	<audio src="'.$enmorning.'" controls></audio>
+	<h3>'.$enetitle.'</h3>
+	<audio src="'.$enevening.'" controls></audio>
+	<h3>'.$cnmasstitle.'</h3>
+	<audio src="'.$cnmass.'" controls></audio>
+	<br/><h3><b>提示：播放音频会损耗较多流量</b></h3></div></center></body><script type="text/javascript" language="javascript" src="/include/googleanalysis.js"></script>');
+	fwrite($fp,getWechatShareScript(ROOT_WEB_URL.'media/vaticanradiowebcast_cn.html','梵蒂冈广播电台',ROOT_WEB_URL.'media/icon.jpg'));
+	fwrite($fp,'</html>');
+	fclose($fp);
+	echo("check vaticanradio(".date("Y-m-d",time()).")");
+?>
