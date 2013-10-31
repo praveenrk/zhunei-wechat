009b2a006db95658d88e661360a5265788bb75e7
liyake04@gmail.com <liyake04@gmail.com@112a1d2d-8a66-39b0-3757-4046d3d5969e>
2013-10-31 13:47:00 +0000

    修改百度统计的代码

--- trunk/web/include/googleanalysis.js
+++ trunk/web/include/googleanalysis.js
@@ -2,7 +2,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "//hm.baidu.com/hm.js?fcafbb35e0f0d791b1af7061f2554738";
+  hm.src = "//hm.baidu.com/hm.js?ba70a76114ed3d7071ccc0b18a16bc3c";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
