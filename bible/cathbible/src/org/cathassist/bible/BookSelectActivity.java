ea9bf704cf377c9b99bb2d652d9d30958628d2de
dfu.friend@gmail.com <dfu.friend@gmail.com@112a1d2d-8a66-39b0-3757-4046d3d5969e>
2013-11-29 15:59:23 +0000

    完善了书卷、章节选择界面和书签内容界面

--- bible/cathbible/src/org/cathassist/bible/BookSelectActivity.java
+++ bible/cathbible/src/org/cathassist/bible/BookSelectActivity.java
@@ -3,10 +3,13 @@ package org.cathassist.bible;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.MenuItem;
@@ -20,6 +23,8 @@ import java.util.Map;
 
 public class BookSelectActivity extends SherlockActivity implements OnItemClickListener {
     ListView mList;
+    GridView mGrid;
+    List<Map<String,String>> mData;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,16 +35,80 @@ public class BookSelectActivity extends SherlockActivity implements OnItemClickL
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("选择卷");
 
-        mList = (ListView) findViewById(R.id.list);
-        mList.setDivider(null);
+        mGrid = (GridView) findViewById(R.id.grid);
 
+        mData = getData();
         SimpleAdapter adapter = new SimpleAdapter(this,
-                GetData(),
+                mData,
                 R.layout.book_select_item,
-                new String[]{"book"},
-                new int[]{R.id.list});
-        mList.setAdapter(adapter);
-        mList.setOnItemClickListener(this);
+                new String[] {"abbr","name"},
+                new int[] {R.id.abbr,R.id.name})
+        {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                ViewHolder holder;
+                if (convertView == null) {
+                    convertView = super.getView(position, convertView, parent);
+                    holder = new ViewHolder();
+                    holder.abbr = (TextView) convertView.findViewById(R.id.abbr);
+                    holder.name = (TextView) convertView.findViewById(R.id.name);
+                    convertView.setTag(holder);
+                } else {
+                    holder = (ViewHolder) convertView.getTag();
+                }
+
+                int book = position + 1;
+                int color;
+                switch (VerseInfo.getBookType(book)) {
+                    case VerseInfo.PENTATEUCH:
+                        color = getResources().getColor(R.color.yellow);
+                        break;
+                    case VerseInfo.HISTORY:
+                        color = getResources().getColor(R.color.cyan);
+                        break;
+                    case VerseInfo.WISDOM_AND_POETRY:
+                        color = getResources().getColor(R.color.gray);
+                        break;
+                    case VerseInfo.MAJOR_PROPHETS:
+                        color = getResources().getColor(R.color.light_yellow);
+                        break;
+                    case VerseInfo.MINOR_PROPHETS:
+                        color = getResources().getColor(R.color.pink);
+                        break;
+                    case VerseInfo.GOSPELS:
+                        color = getResources().getColor(R.color.yellow);
+                        break;
+                    case VerseInfo.ACTS_OF_APOSTLES:
+                        color = getResources().getColor(R.color.cyan);
+                        break;
+                    case VerseInfo.PAULINE_EPISTLES:
+                        color = getResources().getColor(R.color.gray);
+                        break;
+                    case VerseInfo.GENERAL_EPISTLES:
+                        color = getResources().getColor(R.color.light_yellow);
+                        break;
+                    case VerseInfo.APOCALYPTIC:
+                        color = getResources().getColor(R.color.pink);
+                        break;
+                    default:
+                        color = getResources().getColor(R.color.white);
+                        break;
+                }
+                convertView.setBackgroundColor(color);
+
+                holder.abbr.setText(mData.get(position).get("abbr"));
+                holder.name.setText(mData.get(position).get("name"));
+                return convertView;
+            }
+        };
+
+        mGrid.setAdapter(adapter);
+        mGrid.setOnItemClickListener(this);
+    }
+
+    private static final class ViewHolder {
+        public TextView abbr;
+        public TextView name;
     }
 
     @Override
@@ -49,14 +118,14 @@ public class BookSelectActivity extends SherlockActivity implements OnItemClickL
     }
 
     @Override
-    public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-        CommonPara.currentBook = arg2 + 1;
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        CommonPara.currentBook = position + 1;
         CommonPara.currentChapter = 1;
         CommonPara.currentSection = 0;
         CommonPara.bibleMp3Pos = 0;
 
+        view.setBackgroundColor(getResources().getColor(R.color.light_blue));
         Intent intent = new Intent();
-
         intent.setClass(this, ChapterSelectActivity.class);
         startActivity(intent);
     }
@@ -71,12 +140,12 @@ public class BookSelectActivity extends SherlockActivity implements OnItemClickL
         return super.onOptionsItemSelected(item);
     }
 
-    private List<Map<String, String>> GetData() {
+    private List<Map<String, String>> getData() {
         List<Map<String, String>> data = new ArrayList<Map<String, String>>();
         for (int i = 1; i <= 73; i++) {
-            HashMap<String, String> map = new HashMap<String, String>();
-            map.put("book", VerseInfo.CHN_NAME[i] + "  "
-                    + VerseInfo.ENG_NAME[i]);
+            Map<String, String> map = new HashMap<String, String>();
+            map.put("name", VerseInfo.CHN_NAME[i]);
+            map.put("abbr",VerseInfo.CHN_ABBR[i]);
             data.add(map);
         }
         return data;
