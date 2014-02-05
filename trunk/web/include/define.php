3005e9183797bc5440af9c591defbbeb0ed567c2
liyake04@gmail.com <liyake04@gmail.com@112a1d2d-8a66-39b0-3757-4046d3d5969e>
2014-02-05 01:34:30 +0000

    修复微信朋友圈的分享功能

--- trunk/web/include/define.php
+++ trunk/web/include/define.php
@@ -2,9 +2,65 @@
 define("ROOT_WEB_URL", "http://www.cathassist.org/");
 
 function getWechatShareScript($link,$title,$imgurl)
-{
-	return '<script language="javascript" type="text/javascript">var contentModel={
-	"img_url": "'.$imgurl.'", "img_width": "","img_height": "","link": "'.$link.'", "desc": "'.$title.'", 
-    "title": "'.$title.'", "src": "天主教小助手" };</script><script language="JavaScript" src="http://mat1.gtimg.com/www/js/newsapp/wechat/wechat20130809_min.js" type="text/javascript" charset="utf-8"></script>';
+{	
+	return '<script language="javascript" type="text/javascript">
+var imgUrl = "'.$imgurl.'";
+var lineLink = "'.$link.'";
+var descContent = "'.$title.'";
+var shareTitle = "'.$title.'";
+var appid = "wxe95ac80cef656f14";
+
+function shareFriend() {
+    WeixinJSBridge.invoke(\'sendAppMessage\',{
+                            "appid": appid,
+                            "img_url": imgUrl,
+                            "img_width": "640",
+                            "img_height": "640",
+                            "link": lineLink,
+                            "desc": descContent,
+                            "title": shareTitle
+                            }, function(res) {
+                            _report(\'send_msg\', res.err_msg);
+                            })
+}
+function shareTimeline() {
+    WeixinJSBridge.invoke(\'shareTimeline\',{
+                            "img_url": imgUrl,
+                            "img_width": "640",
+                            "img_height": "640",
+                            "link": lineLink,
+                            "desc": descContent,
+                            "title": shareTitle
+                            }, function(res) {
+                            _report(\'timeline\', res.err_msg);
+                            });
+}
+function shareWeibo() {
+    WeixinJSBridge.invoke(\'shareWeibo\',{
+                            "content": descContent,
+                            "url": lineLink,
+                            }, function(res) {
+                            _report(\'weibo\', res.err_msg);
+                            });
+}
+
+// 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
+document.addEventListener(\'WeixinJSBridgeReady\', function onBridgeReady() {
+        // 发送给好友
+        WeixinJSBridge.on(\'menu:share:appmessage\', function(argv){
+            shareFriend();
+            });
+
+        // 分享到朋友圈
+        WeixinJSBridge.on(\'menu:share:timeline\', function(argv){
+            shareTimeline();
+            });
+
+        // 分享到微博
+        WeixinJSBridge.on(\'menu:share:weibo\', function(argv){
+            shareWeibo();
+            });
+        }, false);
+</script>';
 }
 ?>
