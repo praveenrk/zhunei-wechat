09378a7f82fae3f05e78dae9e195c2e87c6f1246
liyake04@gmail.com <liyake04@gmail.com@112a1d2d-8a66-39b0-3757-4046d3d5969e>
2014-02-12 10:01:30 +0000

    1、修复播放音频导致页面跳转的bug 2、修复『天主教小助手』app服务器端 广播和stuff 的返回值

--- trunk/web/media/getradio.php
+++ trunk/web/media/getradio.php
@@ -5,9 +5,10 @@
 	$root = $xmlDoc->documentElement;
 	
 	$ret = null;
-	$ret['mp3'] = $root->getElementsByTagName('MusicUrl')->item(0)->textContent;
-	$ret['title'] = $root->getElementsByTagName('Title')->item(0)->textContent;
-	$ret['desc'] = $root->getElementsByTagName('Description')->item(0)->textContent;
+	$ret['mp3'] = '';
+	$ret['url'] = $root->getElementsByTagName('MusicUrl')->item(0)->textContent;
+	$ret['title'] = '中文广播('.$root->getElementsByTagName('Description')->item(0)->textContent.')';
+	$ret['desc'] = '<br/><br/><br/><br/><br/>因软件有bug，会造成后台消耗流量，所以暂停梵蒂冈广播的收听。我们会尽快更新的，造成不便，深表遗憾！愿天主降幅你！';
 	
 	echo $_GET['callback'].'('.json_encode($ret).')';
 ?>
