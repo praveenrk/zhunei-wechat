d99acccce2e80093764e8c1ca86c514e59865e45
tashigaofei <tashigaofei@112a1d2d-8a66-39b0-3757-4046d3d5969e>
2014-03-08 14:21:29 +0000

    init pro

---
+++ trunk/cxradio/ios/CXRadio/CXRadio/CXViewController.h
@@ -0,0 +1,13 @@
+//
+//  CXViewController.h
+//  CXRadio
+//
+//  Created by tashigaofei on 14-3-7.
+//  Copyright (c) 2014年 zhaoyanjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CXViewController : UIViewController
+
+@end
