6fb293836b2bf0b4f5280b7353167e02284f2320
liyake04@gmail.com <liyake04@gmail.com@112a1d2d-8a66-39b0-3757-4046d3d5969e>
2014-06-12 22:56:08 +0000

    add calendar support use cpp

--- trunk/3rd/cpp/calendar/main.cpp
+++ trunk/3rd/cpp/calendar/main.cpp
@@ -4,32 +4,36 @@
 using namespace std;
 using namespace CathAssist::Calendar;
 
-int main(int _Argc, char ** _Argv, char ** _Env)
+void printYear(const int& year)
 {
-    /*
-     示例代码,2014年的输出
-     
-    //初始化Calendar，只需在程序入口调用一次
-    Calendar::initCalendar();
-    
-    Date d(2014,1,1);
-    for(int i=0;i<366;++i)
+    Date d(year,1,1);
+    int countDay = Date::isLeapYear(year) ? 366 : 365;
+    for(int i=0;i<countDay;++i)
     {
         Date t = d.addDays(i);
         LiturgicDay ld = Calendar::getLiturgicDay(t.year(), t.month(), t.day());
         cout<<ld.toLiturgicString()<<endl;
     }
+}
+
+
+int main(int _Argc, char ** _Argv, char ** _Env)
+{
+    //初始化Calendar，只需在程序入口调用一次
+    Calendar::initCalendar();
     
-    //释放Calendar资源，只需在程序结束时调用一次（放置内存泄露）
-    Calendar::releaseCalendar();
-    */
+    //示例代码,2014年的输出
+    printYear(2014);
     
-    
+    /*
 	for(int i=2000;i<2010;++i)
 	{
 		LiturgicYear y(i);
 		cout<<y.toString()<<endl;
 	}
+    */
     
+    //释放Calendar资源，只需在程序结束时调用一次（放置内存泄露）
+    Calendar::releaseCalendar();
 	return 0;
 }
