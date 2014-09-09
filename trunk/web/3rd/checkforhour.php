d9edc671c88585020c7a8731b3f2ab66a62b7e03
liyake04@gmail.com <liyake04@gmail.com@112a1d2d-8a66-39b0-3757-4046d3d5969e>
2014-09-09 03:26:38 +0000

    修复梵蒂冈电台改版后造成小助手无法抓取的bug

--- trunk/web/3rd/checkforhour.php
+++ trunk/web/3rd/checkforhour.php
@@ -5,7 +5,7 @@
 	echo(file_get_contents(ROOT_WEB_URL.'vaticanacn/checkrss.php'));
 	
 	echo('<h1>获取梵蒂冈电台广播</h1>');
-	echo(file_get_contents(ROOT_WEB_URL.'media/checkrss.php'));
+	echo(file_get_contents(ROOT_WEB_URL.'media/checkrss1.php'));
 	
 	echo('<h1>获取信德网文章</h1>');
 	echo(file_get_contents(ROOT_WEB_URL.'faithlife/caiji.php'));
