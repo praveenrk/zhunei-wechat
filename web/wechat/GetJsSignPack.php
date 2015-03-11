4bd5a1c9aa4c9e273f0c88df6a9f1c206362db7d
liyake04@gmail.com <liyake04@gmail.com@112a1d2d-8a66-39b0-3757-4046d3d5969e>
2015-03-11 12:42:13 +0000

    修复分享的bug

--- web/wechat/GetJsSignPack.php
+++ web/wechat/GetJsSignPack.php
@@ -6,7 +6,7 @@ if(array_key_exists("type",$_GET))
 	if($_GET["type"]=="json")
 	{
 		$jssdk = new JSSDK("", "");
-		$signPackage = $jssdk->GetSignPackage($_GET["url"]);
+		$signPackage = $jssdk->GetSignPackage(urldecode($_GET["url"]));
 
 		$ret = json_encode($signPackage);
 		//echo $_GET['callback'].'('.$ret.')';
